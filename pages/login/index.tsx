7cb8f629e76c062a688492213cb7f13357ebbc0f
ishivamshukla <ishivamshukl@gmail.com>
2023-04-05 17:37:53 +0530

    first commit

---
+++ pages/login/index.tsx
@@ -0,0 +1,90 @@
+import { useLogin } from "@refinedev/core";
+
+import { Box, Button, Text, VStack } from "@chakra-ui/react";
+import { ThemedTitle } from "@refinedev/chakra-ui";
+
+import { useTranslate } from "@refinedev/core";
+import { GetServerSideProps } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { AppIcon } from "src/components/app-icon";
+
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]";
+
+export default function Login() {
+  const { mutate: login } = useLogin();
+
+  const t = useTranslate();
+
+  return (
+    <Box
+      sx={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <VStack spacing="10" align="stretch">
+        <ThemedTitle
+          collapsed={false}
+          wrapperStyles={{
+            fontSize: "22px",
+          }}
+          text="refine Project"
+          icon={<AppIcon />}
+        />
+
+        <Button
+          style={{ width: "240px" }}
+          colorScheme="blue"
+          onClick={() => login({})}
+        >
+          {t("pages.login.signin", "Sign in")}
+        </Button>
+
+        <Text
+          justifyContent="center"
+          display="inherit"
+          fontSize="12px"
+          color="gray"
+        >
+          Powered by
+          <img
+            style={{ padding: "0 5px" }}
+            alt="Auth0"
+            src="https://refine.ams3.cdn.digitaloceanspaces.com/superplate-auth-icons%2Fauth0-2.svg"
+          />
+          Auth0
+        </Text>
+      </VStack>
+    </Box>
+  );
+}
+
+Login.noLayout = true;
+
+export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
+  const session = await getServerSession(context.req, context.res, authOptions);
+
+  const translateProps = await serverSideTranslations(context.locale ?? "en", [
+    "common",
+  ]);
+
+  if (session) {
+    return {
+      props: {},
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      ...translateProps,
+    },
+  };
+};
